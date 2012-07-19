c228bd69c33b767acf23d7ff02c2d0811cec013f
Dean Eckles <deckles@deckles-mbp.local>
2012-07-19 11:38:43 -0700

    Change function names.

--- multiway_boot.R
+++ multiway_boot.R
@@ -4,15 +4,15 @@ r.double.or.nothing <- function(n) {
   2 * rbinom(n, 1, .5)
 }
 
-online.multiway.boot <- function(statistic, R, N,
-                               groups = as.matrix(1:N),
-                               verbose = FALSE,
-                               RNG = r.double.or.nothing,
-                               ...) {
+multiway.boot <- function(statistic, R, N,
+                          groups = as.matrix(1:N),
+                          verbose = FALSE,
+                          RNG = r.double.or.nothing,
+                          ...) {
   groups.num <- apply(groups, 2, function(x) as.numeric(as.factor(x)))
   N.groups <- apply(groups, 2, function(x) length(unique(x)))
   N.groupingFactors <- ncol(groups)
-  
+
   llply(1:R, function(i) {
     W <- matrix(nrow = 0, ncol = N)
     for (j in 1:N.groupingFactors) {
